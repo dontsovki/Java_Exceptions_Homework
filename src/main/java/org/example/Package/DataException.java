069843ac3e35cb4b6a33685e5976dc99fa80e1b1
Artem.don <dontsovki@gmail.com>
2023-11-10 17:22:39 +0300

    Initial commit

---
+++ src/main/java/org/example/Package/DataException.java
@@ -0,0 +1,12 @@
+package org.example.Package;
+
+public class DataException extends Exception{
+    public DataException() {
+    }
+
+    public void dataException(String i) {
+        System.out.println("Exception: DataNotCorrectFormat");
+        System.out.printf("It`s not correct format: %s", i);
+        System.out.println();
+    }
+}
